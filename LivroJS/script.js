d7d2f5846f44eec2a85d3da17c292d736eac79e3
Vinicius De Filippo <viniciusdefilippo@gmail.com>
2023-06-07 22:40:25 -0300

    add Readme

--- LivroJS/script.js
+++
@@ -1,9 +0,0 @@
-function Book(title, pages, isbn){
-    this.title = title;
-    this.pages = pages;
-    this.isbn = isbn;
-}
-
-var book = new Book('Livro 1', 'pag', 'isbn')
-
-console.log(book.title)
